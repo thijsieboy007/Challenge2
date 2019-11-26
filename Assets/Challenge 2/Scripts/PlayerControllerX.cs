d5bec4152af12768cc28d5bff7eb35575e144a0a
Thijs Lausegger <326550@student.mboutrecht.nl>
2019-11-26 10:18:56 +0100

    no message

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private bool canShoot = true;
+    
+    public GameObject dogPrefab;
+    private float coolDown = 1.0f;
+    private float nextFire = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + coolDown;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
